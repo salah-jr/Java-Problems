a6cd4d8fabcdb64a28647368e19dfd452d1ae4a5
salah-jr <mohamed.salah21500@gmail.com>
2022-03-24 19:07:08 +0200

    clear master

---
+++ src/com/salah/Main.java
@@ -0,0 +1,21 @@
+package com.salah;
+
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+
+
+    }
+
+
+    private static void swap(int[] array, int j, int i) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+
+
+}
