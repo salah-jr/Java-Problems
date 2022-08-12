6e5995b19356544455bf8db74df9db73af61c10e
salah-jr <mohamed.salah21500@gmail.com>
2022-08-12 18:00:42 +0200

    Array dublicates

--- src/com/salah/FindArrayDublicates.java
+++ src/com/salah/FindArrayDublicates.java
@@ -1,2 +1,30 @@
-package com.salah;public class FindArrayDublicates {
+package com.salah;
+
+
+import java.util.HashSet;
+
+public class FindArrayDublicates {
+
+    int[] array;
+    public FindArrayDublicates(int[] array){
+        this.array = array;
+    }
+
+    public void result(){
+        HashSet<Integer> hash = new HashSet<>();
+
+        int i = 0;
+        StringBuilder output = new StringBuilder();
+
+        while (i <= array.length - 1) {
+            if (hash.contains(array[i])) {
+                output.append(array[i]).append(" ");
+            }
+
+            hash.add(array[i]);
+            i++;
+        }
+
+        System.out.println("The duplicates numbers are : " + output);
+    }
 }
--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -6,30 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] array = new int[] { 10, 17, 17, 9, 8, 7, 15, 30, 20, 19, 19, 35, 35 };
+        int[] array = new int[]{19, 10, 17, 10, 9, 8, 7, 15, 17, 30, 20, 19, 35, 35};
 
-        HashSet<Integer> hash = new HashSet<>();
-
-        int i = 0;
-        StringBuilder output = new StringBuilder();
-
-        while (i <= array.length - 1) {
-            if (hash.contains(array[i])) {
-                output.append(array[i]).append(" ");
-            }
-
-            hash.add(array[i]);
-            i++;
-        }
-
-        System.out.println("The duplicates numbers are : " + output);
+        FindArrayDublicates obj = new FindArrayDublicates(array);
+        obj.result();
 
     }
 
-    // private static void swap(int[] array, int j, int i) {
-    // int temp = array[j];
-    // array[j] = array[i];
-    // array[i] = temp;
-    // }
+    private static void swap(int[] array, int j, int i) {
+        int temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
 
 }
