5592788d67ea8dc0ce86fb1a431b88088219ea6f
salah-jr <mohamed.salah21500@gmail.com>
2022-08-13 16:09:34 +0200

    solving TwoSum problem

--- src/com/salah/TwoSum.java
+++ src/com/salah/TwoSum.java
@@ -1,2 +1,23 @@
-package com.salah;public class TwoSum {
+package com.salah;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (h.containsKey(nums[i])) {
+                return new int[]{h.get(nums[i]), i};
+            }
+
+            int remain = target - nums[i];
+            h.put(remain, i);
+        }
+
+        return nums;
+    }
+
 }
