55418f08683b9b17e80c14a28fc0634e9ca8911a
salah-jr <mohamed.salah21500@gmail.com>
2022-08-19 15:34:01 +0200

    Solve Running sum problem

---
+++ src/com/salah/RunningSum.java
@@ -0,0 +1,16 @@
+package com.salah;
+
+public class RunningSum {
+
+    public int[] runningSum(int[] nums) {
+        int[] results = new int[nums.length];
+        results[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            results[i] = nums[i] + results[i - 1];
+        }
+
+        return results;
+    }
+
+}
--- src/com/salah/TwoSum.java
+++ src/com/salah/TwoSum.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
 
-        HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> h = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
 
@@ -19,5 +19,4 @@ public class TwoSum {
 
         return nums;
     }
-
 }
