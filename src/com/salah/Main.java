3891567dd9e1d7fcc3e78c901e16ebca47d221e6
salah-jr <mohamed.salah21500@gmail.com>
2022-09-07 00:07:29 +0200

    Add MoveZeros

--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] array = new int[]{19, 10, 17, 10, 9, 0, 7, 0, 0, 30, 20, 19, 35, 35};
+        int[] array = new int[]{19, 10, 17, 10, 9, 0, 7, 0, 0, 0, 20, 19, 35, 35};
         int[] sortedArray = new int[]{1,5,6,7,9,11,14,15,19,20};
 
         MoveZeros obj = new MoveZeros();
