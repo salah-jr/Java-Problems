144694eed9508cfa71e92226074fdee2ca82d72e
salah-jr <mohamed.salah21500@gmail.com>
2022-11-20 19:03:54 +0200

    Add remove element problem

--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -1,20 +1,14 @@
 package com.salah;
 
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(RomanToInteger.romanToInt("IX"));
-        System.out.println(RomanToInteger.romanToInt("IV"));
-        System.out.println(RomanToInteger.romanToInt("XL"));
-        System.out.println(RomanToInteger.romanToInt("XC"));
-        System.out.println(RomanToInteger.romanToInt("CD"));
-        System.out.println(RomanToInteger.romanToInt("CM"));
-        System.out.println(RomanToInteger.romanToInt("LVIII"));
-        System.out.println(RomanToInteger.romanToInt("III"));
-        System.out.println(RomanToInteger.romanToInt("MCMXCIV"));
-
-
+        int[] nums={0,1,2,2,3,0,4,2};
+        System.out.println(RemoveElement.removeElement(nums, 2));
     }
 
     private static void swap(int[] array, int j, int i) {
