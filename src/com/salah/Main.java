2ac9bf401fa91e4da2d15865ecdb4a4121b1b3c7
salah-jr <mohamed.salah21500@gmail.com>
2022-11-21 15:02:09 +0200

    add merge sorted arrays

--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -1,14 +1,11 @@
 package com.salah;
 
-
-import java.sql.Array;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums={0,1,2,2,3,0,4,2};
-        System.out.println(RemoveElement.removeElement(nums, 2));
+
     }
 
     private static void swap(int[] array, int j, int i) {
