4ac4f1693642af48a3bf5ab3c121ae0781e213b0
salah-jr <mohamed.salah21500@gmail.com>
2022-11-24 16:59:37 +0200

    add third maximum number problem

--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -1,12 +1,17 @@
 package com.salah;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
         int [] nums1 = {4,9,5};
         int [] nums2 = {9,4,9,8,4};
-
-
+        Map<Integer, Integer> map = new HashMap<>();
+        map.get(map.size() - 3);
     }
 
     private static void swap(int[] array, int j, int i) {
