515394628228a4fb4ccf05ec174da59b4ba38bfd
salah-jr <mohamed.salah21500@gmail.com>
2022-11-25 16:23:54 +0200

    add First unique character

---
+++ src/com/salah/FirstUniqueCharacter.java
@@ -0,0 +1,16 @@
+package com.salah;
+
+public class FirstUniqueCharacter {
+
+    public int firstUniqChar(String s) {
+        int ans = Integer.MAX_VALUE;
+        for (char c = 'a'; c <= 'z'; c++) {
+            int index = s.indexOf(c);
+            if (index != -1 && index == s.lastIndexOf(c)) {
+                ans = Math.min(ans, index);
+            }
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -1,15 +1,13 @@
 package com.salah;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int [] nums1 = {4,9,5};
-        int [] nums2 = {9,4,9,8,4};
+        int[] nums1 = {4, 9, 5};
+        int[] nums2 = {9, 4, 9, 8, 4};
+        String s = "I love salah";
     }
 
     private static void swap(int[] array, int j, int i) {
