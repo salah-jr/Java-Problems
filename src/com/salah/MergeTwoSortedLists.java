7d2fdd7f398b091628a8ff89292a38683089231a
salah-jr <mohamed.salah21500@gmail.com>
2022-11-26 16:26:43 +0200

    Merge two linked lists

--- src/com/salah/MergeTwoSortedLists.java
+++ src/com/salah/MergeTwoSortedLists.java
@@ -2,14 +2,27 @@ package com.salah;
 
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
-        if(list1.val < list2.val){
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        } else{
-            list2.next = mergeTwoLists(list1, list2.next);
-            return list2;
+        ListNode dummy= new ListNode(-1);
+        ListNode head = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val)
+            {
+                dummy.next = list1;
+                list1 = list1.next;
+            } else{
+                dummy.next = list2;
+                list2 = list2.next;
+            }
+            dummy = dummy.next;
         }
+
+        if(list1 != null){
+            dummy.next = list1;
+        } else{
+            dummy.next = list2;
+        }
+
+        return head.next;
     }
 }
