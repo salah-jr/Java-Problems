02819a4cc4daf28b143edd5733abf6d441c45ff3
salah-jr <mohamed.salah21500@gmail.com>
2022-11-27 19:43:58 +0200

    Update reverse linked list

--- src/com/salah/ReverseLinkedList.java
+++ src/com/salah/ReverseLinkedList.java
@@ -1,28 +1,17 @@
 package com.salah;
 
+// We can solve it with recursion or with a stack.
 public class ReverseLinkedList {
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
+        ListNode prev = null;
+
+        while(head != null){
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
         }
 
-        ListNode p1 = head;
-        ListNode p2 = p1.next;
-        ListNode p3 = p2.next;
-
-        // Making first node last node
-        head.next = null;
-
-        while(p3 != null){
-            p2.next = p1;
-            p1 = p2;
-            p2 = p3;
-            p3 = p3.next;
-        }
-
-        // Making the first node in the reversed list points to the second node.
-        p2.next = p1;
-
-        return p2;
+        return prev;
     }
 }
