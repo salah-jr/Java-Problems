95fa93ea372d8f1878c9870f530d7453363d3b3e
salah-jr <mohamed.salah21500@gmail.com>
2022-11-27 20:09:44 +0200

    add remove duplicated from sorted linked list

---
+++ src/com/salah/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,18 @@
+package com.salah;
+
+public class RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = head;
+
+        while(head != null && head.next != null){
+            if(head.val == head.next.val){
+                head.next = head.next.next;
+            } else{
+                head = head.next;
+            }
+
+        }
+
+        return dummy;
+    }
+}
