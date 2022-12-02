8203078fa195a709ae3ae29449f688b7a40551a9
salah-jr <mohamed.salah21500@gmail.com>
2022-12-02 15:08:34 +0200

    add search in binary search tree

--- src/com/salah/SearchInBinarySearchTree.java
+++ src/com/salah/SearchInBinarySearchTree.java
@@ -2,9 +2,27 @@ package com.salah;
 
 public class SearchInBinarySearchTree {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) return null;
-        if(root.val == val) return root;
-        if(root.val < val) return searchBST(root.right, val);
+        if (root == null) return null;
+        if (root.val == val) return root;
+        if (root.val < val) return searchBST(root.right, val);
         else return searchBST(root.left, val);
     }
+
+
+//      Iterative solution
+//
+//    public TreeNode searchBST(TreeNode root, int val) {
+//        if (root == null) return null;
+//        TreeNode cur = root;
+//        while (cur != null) {
+//            if (cur.val == val) return cur;
+//            else if (val < cur.val) {
+//                cur = cur.left;
+//            }
+//            else {
+//                cur = cur.right;
+//            }
+//        }
+//        return null;
+//    }
 }
