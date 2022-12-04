ca48947aebc21ff3ca5969f8a687f0c641baaaa2
salah-jr <mohamed.salah21500@gmail.com>
2022-12-04 13:17:56 +0200

    Add climbing stairs

--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -5,23 +5,9 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums1 = {4, 9, 5};
-        int[] nums2 = {9, 4, 9, 8, 4};
-        String s = "()";
-        int p1 = 0, p2 = s.length()-1;
-        char [] array = s.toCharArray();
-        List<Integer> list = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        System.out.println(false || false);
-        Set<Integer> set = new HashSet<>();
 
     }
 
-    private static void swap(int[] array, int j, int i) {
-        int temp = array[j];
-        array[j] = array[i];
-        array[i] = temp;
-    }
 
 }
   
