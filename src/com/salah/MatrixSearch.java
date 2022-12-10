bd4669b9fcda058ceb410d8268c542ee25c3d96f
salah-jr <mohamed.salah21500@gmail.com>
2022-12-10 19:01:40 +0200

    Add matrix search problem

--- src/com/salah/MatrixSearch.java
+++ src/com/salah/MatrixSearch.java
@@ -1,2 +1,18 @@
-package com.salah;public class MatrixSearch {
+package com.salah;
+
+public class MatrixSearch {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix[i].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
 }
