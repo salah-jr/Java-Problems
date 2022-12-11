5af7b72c1ada3e364d9b9e22f16fddb92bf8fe39
salah-jr <mohamed.salah21500@gmail.com>
2022-12-11 18:27:05 +0200

    Add product of array except self

--- src/com/salah/ProductOfArrayExceptSelf.java
+++ src/com/salah/ProductOfArrayExceptSelf.java
@@ -4,11 +4,14 @@ public class ProductOfArrayExceptSelf {
     public int[] productExceptSelf(int[] nums) {
         int[] answer = new int[nums.length];
 
-        for (int i = 0, tmp = 1; i < nums.length; i++) {
+        int tmp = 1;
+        for (int i = 0; i < nums.length; i++) {
             answer[i] = tmp;
             tmp *= nums[i];
         }
-        for (int i = nums.length - 1, tmp = 1; i >= 0; i--) {
+
+        tmp = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
             answer[i] *= tmp;
             tmp *= nums[i];
         }
