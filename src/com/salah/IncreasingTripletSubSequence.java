4f87f1e8d4f5b6c7e2089b48f795cc45f51af7f4
salah-jr <mohamed.salah21500@gmail.com>
2022-12-11 17:23:03 +0200

    Add increasing triplet sunsequence

---
+++ src/com/salah/IncreasingTripletSubSequence.java
@@ -0,0 +1,13 @@
+package com.salah;
+
+public class IncreasingTripletSubSequence {
+    public boolean increasingTriplet(int[] nums) {
+        int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= small) small = n;
+            else if (n <= big) big = n;
+            else return true;
+        }
+        return false;
+    }
+}
