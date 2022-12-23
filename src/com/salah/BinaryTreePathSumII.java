814ad5b20a3cc537065ace74819f9ed059901c6a
salah-jr <mohamed.salah21500@gmail.com>
2022-12-23 18:17:26 +0200

    Add delete node in binary search tree

--- src/com/salah/BinaryTreePathSumII.java
+++ src/com/salah/BinaryTreePathSumII.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class BinaryTreePathSumII {
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> res = new ArrayList();
-        helper(root, targetSum, new ArrayList<Integer>(), res);
+        List<List<Integer>> res = new ArrayList<>();
+        helper(root, targetSum, new ArrayList<>(), res);
         return res;
     }
 
@@ -16,7 +16,7 @@ public class BinaryTreePathSumII {
 
         sol.add(root.val);
 
-        if(root.left == null && root.right == null && targetSum == root.val) res.add(new ArrayList<Integer>(sol));
+        if(root.left == null && root.right == null && targetSum == root.val) res.add(new ArrayList<>(sol));
         else {
             helper(root.left, targetSum - root.val, sol, res);
             helper(root.right, targetSum - root.val, sol, res);
