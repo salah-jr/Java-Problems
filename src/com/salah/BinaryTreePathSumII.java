65024e09ef9649b3a42b628a26a445caee04fb9a
salah-jr <mohamed.salah21500@gmail.com>
2022-12-23 17:11:15 +0200

    Add path sum II

---
+++ src/com/salah/BinaryTreePathSumII.java
@@ -0,0 +1,27 @@
+package com.salah;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePathSumII {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList();
+        helper(root, targetSum, new ArrayList<Integer>(), res);
+        return res;
+    }
+
+    void helper(TreeNode root, int targetSum, ArrayList<Integer> sol, List<List<Integer>> res)
+    {
+        if(root == null) return;
+
+        sol.add(root.val);
+
+        if(root.left == null && root.right == null && targetSum == root.val) res.add(new ArrayList<Integer>(sol));
+        else {
+            helper(root.left, targetSum - root.val, sol, res);
+            helper(root.right, targetSum - root.val, sol, res);
+        }
+
+        sol.remove(sol.size() - 1);
+    }
+}
