cd2b7ef1e47a446b915471eff04a4e4ceb4d809c
salah-jr <mohamed.salah21500@gmail.com>
2022-12-24 16:51:11 +0200

    Add lowest common ancestor of binary tree

---
+++ src/com/salah/LowestCommonAncestorOfBT.java
@@ -0,0 +1,11 @@
+package com.salah;
+
+public class LowestCommonAncestorOfBT {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q)  return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null)   return root;
+        return left != null ? left : right;
+    }
+}
