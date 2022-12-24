ca937acbd5a3fc6cce41b5c3ffdd6c4bbf6fbc95
salah-jr <mohamed.salah21500@gmail.com>
2022-12-25 00:43:15 +0200

    Add serialize and deserialize  binary tree

--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -7,7 +7,12 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(f(3));
     }
 
+    static int f(int num)
+    {
+        if(num > 0) return num * f(num -1);
+        return 1;
+    }
 }
