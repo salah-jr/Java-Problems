5f5c94b75123c0afa8af1bee089344c924e0fb97
salah-jr <mohamed.salah21500@gmail.com>
2023-01-05 02:17:06 +0200

    Add merge two binary trees

---
+++ src/com/salah/MergeTwoBinaryTrees.java
@@ -0,0 +1,15 @@
+package com.salah;
+
+public class MergeTwoBinaryTrees{
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null) return root2;
+        if(root2 == null) return root1;
+
+        TreeNode newRoot = new TreeNode(root1.val + root2.val);
+
+        newRoot.right = mergeTrees(root1.right , root2.right);
+        newRoot.left = mergeTrees(root1.left , root2.left);
+
+        return newRoot;
+    }
+}
