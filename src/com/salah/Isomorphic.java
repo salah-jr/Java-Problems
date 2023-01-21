da75271610afc8a3ae6798134472ceb8608f81fd
salah-jr <mohamed.salah21500@gmail.com>
2023-01-21 15:50:34 +0200

    Add Isomorphic strings

---
+++ src/com/salah/Isomorphic.java
@@ -0,0 +1,28 @@
+package com.salah;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Isomorphic {
+    public boolean isIsomorphic(String s, String t) {
+        if (s == null || t == null) return false;
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> mapS = new HashMap<Character, Integer>();
+        Map<Character, Integer> mapT = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            int indexS = mapS.getOrDefault(s.charAt(i), -1);
+            int indexT = mapT.getOrDefault(t.charAt(i), -1);
+
+            if (indexS != indexT) {
+                return false;
+            }
+
+            mapS.put(s.charAt(i), i);
+            mapT.put(t.charAt(i), i);
+        }
+
+        return true;
+    }
+}
--- src/com/salah/Main.java
+++ src/com/salah/Main.java
@@ -1,9 +1,5 @@
 package com.salah;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -14,4 +10,4 @@ public class Main {
         if(n < 2) return n;
         return fibo(n-1) + fibo(n-2);
     }
-}  
+}
